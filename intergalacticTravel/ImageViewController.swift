45bd7867d8b1e16c160cc0674cc14c421e99ff38
student3 <student3@jh-mbair-158-14.d214.org>
2015-11-09 14:47:47 -0600

    added stuff

--- intergalacticTravel/ImageViewController.swift
+++ intergalacticTravel/ImageViewController.swift
@@ -9,27 +9,23 @@
 import UIKit
 
 class ImageViewController: UIViewController {
-
+    
+    var isBlueStar: Bool?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        if isBlueStar!
+        {
+            self.view.backgroundColor = UIColor.blueColor()
+            starImageView. image = UIimage(named: "blueStar"
+        }
+        else
+        {
+            self.view.backgroundColor = UIColor.redColor()
+            starImageView.image = UIImage(named: "redDwarf")
+        }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+}
+   
 }
